1e4b64bf5e62b982e357b88e582482d68023b5fc
ZeroLoo <344025804@qq.com>
2018-08-29 23:08:30 +0800

    17p

--- app/api/v1/client.py
+++ app/api/v1/client.py
@@ -4,20 +4,27 @@ from flask import request
 from app.libs.enums import ClientTypeEnum
 from app.libs.redprint import Redprint
 from app.models.user import User
-from app.validators.forms import ClientForm
+from app.validators.forms import ClientForm, UserEmailForm
 
 __author__ = 'ZeroLoo'
 
-api=Redprint('client')
+api = Redprint('client')
 
 api.route('/register')
-def create_client():
-    data=request.json
-    form=ClientForm(data=data)
-    if form.validate():
-        promise={
-            ClientTypeEnum.USER_EMAIL:__register_user_by_email
-        }
 
-def __register_user_by_email(form):
-    User.register_by_email(form.account.data,form.secret.data)
+
+def create_client():
+    data = request.json
+    form = ClientForm(data=data)
+    if form.validate():
+        promise = {
+            ClientTypeEnum.USER_EMAIL: __register_user_by_email
+        }
+        promise[form.type.data]()
+
+
+
+def __register_user_by_email():
+    form = UserEmailForm(request.json)
+    if form.validate():
+        User.register_by_email(form.nickname, form.account.data, form.secret.data)
